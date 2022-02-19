096d2f78c4869a3ca29b200b0aa33b839aa8149a
billg <billg@MSI>
2022-02-19 18:52:06 +0300

    Add project files.

---
+++ 170222_KelimeTahminOyunu/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _170222_KelimeTahminOyunu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            string sozcuk = "abcçdefghıijklmnoöprsştuüvyzABCÇDEFGHIİJKLMOÖPRSŞTUÜVYZ";
+            string rastgeleKelime = " ";
+            string harfTahmin;
+            for (int i = 0; i < 5; i++)
+            {
+                rastgeleKelime=rastgeleKelime+sozcuk[rnd.Next(sozcuk.Length)];
+            }
+            Console.WriteLine(rastgeleKelime);
+            int sayac=rastgeleKelime.Length;    
+            while (sayac > 0)
+            { basla:
+                Console.WriteLine( "Lütfen bir harf giriniz.");
+                harfTahmin = Console.ReadLine();
+                if(rastgeleKelime.Contains(harfTahmin))
+                {
+                    Console.WriteLine("Tebrikler {0} harfi listemizde mevcuttur.",harfTahmin);
+                    break;
+
+                }
+                else
+                {Console.WriteLine("Tahmininiz hatalıdır, lütfen tekrar deneyiniz.");
+                    goto basla;
+                }
+                    
+            }
+            Console.ReadLine();
+        }
+    }
+}
